dd2d111c8c0f2ef322a165541631801d6b5373f0
jinseungbok <92.bokjs@gmail.com>
2025-08-18 14:30:18 +0900

    [docs]:250818_feed delete

--- src/services/feedService.js
+++ src/services/feedService.js
@@ -5,3 +5,5 @@ const path = '/feed';
 export const postFeed = data => axios.post(path, data).catch(e => e.response);
 
 export const getFeedList = params => axios.get(path, { params }).catch(e => e.response);;
+
+export const deleteFeed = params => axios.delete(path, { params});
